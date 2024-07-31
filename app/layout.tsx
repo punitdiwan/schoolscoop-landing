53f970233033295fa3ccfd10252afe6b5a33964e
punitdiwan <49054503+punitdiwan@users.noreply.github.com>
2024-07-31 11:57:31 +0530

    Updateed OG Image Properties
    Updateed OG Image Properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
         <title>Best school software in Madhya Pradesh</title>
         <meta name="description" content="EduSparsh provides the best school management in MP that helps children, parents, and teachers to stay connected. To transition to the digital world at your school, use this school mobile app." />
         <meta property="og:title" content="Best school software in Madhya Pradesh" />
-        <meta property="og:description" content="The React Framework for the Web" />
-        <meta property="og:url" content="https://www.edusparsh.com/" />
+        <meta property="og:description" content="Best school software in Madhya Pradesh" />
+        <meta property="og:url" content="https://www.edusparsh.com" />
         <meta property="og:site_name" content="EduSparsh" />
         <meta property="og:locale" content="en_US" />
+        <meta property="og:image:type" content="image/jpeg">
         <meta property="og:image" content="https://www.edusparsh.com/og-image.jpg" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
