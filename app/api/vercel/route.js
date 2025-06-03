f196e12c0bc8a15184fb3b3e9f5f7a1730eabfae
punitdiwan <punitdiwan@gmail.com>
2025-06-03 18:27:20 +0530

    date optimise

--- app/api/vercel/route.js
+++ app/api/vercel/route.js
@@ -6,16 +6,13 @@ export async function GET(request) {
 
   
   const token = process.env.VERCEL_TOKEN;
-  
-  // Get today's date
-  const today = new Date();
 
   // Set 'to' date (end of today)
-  const to = new Date(today);
+  const to = new Date();
   to.setHours(23, 59, 59, 999);
 
   // Set 'from' date (29 days ago, start of the day)
-  const from = new Date(today);
+  const from = new Date();
   from.setDate(from.getDate() - 29);
   from.setHours(0, 0, 0, 0);
 
