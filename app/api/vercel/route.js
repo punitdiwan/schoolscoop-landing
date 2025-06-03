aa5a22e55c380d1d78dcca9201d120c4ab234713
punitdiwan <punitdiwan@gmail.com>
2025-06-03 18:04:34 +0530

    date fix

--- app/api/vercel/route.js
+++ app/api/vercel/route.js
@@ -6,16 +6,16 @@ export async function GET(request) {
 
   
   const token = process.env.VERCEL_TOKEN;
+  
   // Get today's date
-  // const today = new Date();
+  const today = new Date();
 
   // Set 'to' date (end of today)
   const to = new Date(today);
   to.setHours(23, 59, 59, 999);
 
-  // Set 'from' date (one month ago - 1 day)
+  // Set 'from' date (29 days ago, start of the day)
   const from = new Date(today);
-  // Set 'from' date (30days)
   from.setDate(from.getDate() - 29);
   from.setHours(0, 0, 0, 0);
 
@@ -23,7 +23,7 @@ export async function GET(request) {
   const fromISO = from.toISOString();
   const toISO = to.toISOString();
 
-  // Construct the full URL
+  // Construct the Vercel Analytics URL
   const url = `https://vercel.com/api/web-analytics/stats?&limit=250&type=path&projectId=edusparsh&teamId=team_qrXJCkE6eCryc3LyWQhmLlwJ&from=${encodeURIComponent(fromISO)}&to=${encodeURIComponent(toISO)}`;
 
   try {
